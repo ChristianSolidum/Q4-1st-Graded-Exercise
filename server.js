a643e77667cd223c2cf7d72fe88d9c216271bb8a
Christian <b2028crcsolidum@pshs.edu.ph>
2025-03-27 00:40:15 +0800

    final

--- server.js
+++ server.js
@@ -1,103 +1,98 @@
 // Loads the express module
 const express = require("express");
 const hbs = require("hbs");
-
 const bodyParser = require("body-parser");
-
 const path = require("path");
 
-//Creates our express server
+// Creates our express server
 const app = express();
 
-//Serves static files (we need it to import a css file)
+// Serves static files (we need it to import a CSS file)
 app.use(express.static(path.join(__dirname, "public")));
+
+// Sets Handlebars as the view engine
 app.set("view engine", "hbs");
+
+// Middleware to parse form data from POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sets a basic route
-
-// Render the initial page with the number input form
+// Redirects "/happy" route to "index" (ensure index.hbs exists)
 app.get("/happy", (req, res) => {
-  res.redirect("index"); // Ensure you have index.hbs
+  res.redirect("index");
 });
 
-// Create express route binder for draw.hbs and get the data from the url as parameters
-// that came from index.hbs
-
-
-
+// Handles POST requests from the form submission on "index.hbs"
 app.post("/happy", (req, res) => {
-  const { myName, gender, number } = req.body;
-  let names = [];
-  let singers = [];
+  const { myName, gender, number } = req.body; // Extracts form data
+  let names = []; // Stores all entered names
+  let singers = []; // Stores names of guests who will sing
 
-  for (let i = 0; i<number; i++){
-      let name = req.body[`name${i+1}`];
-      names.push(name);
-      const isGoing = req.body[`checkbox${i+1}`] ? true : false; // checks whether a close friend/relative is going or not
-    if (isGoing == true) {
-      singers.push(name); // if the guest is going, add them to the singers array
+  // Loops through the number of guests and collects their names
+  for (let i = 0; i < number; i++) {
+    let name = req.body[`name${i + 1}`]; // Retrieves each guest's name
+    names.push(name);
+    
+    // Checks if the guest is attending (checkbox is checked)
+    const isGoing = req.body[`checkbox${i + 1}`] ? true : false;
+    if (isGoing) {
+      singers.push(name); // Adds guest to singers list if attending
     }
   }
 
-  let infos = {};
-  let going = ""
+  let infos = {}; // Stores whether each guest is attending
+  let going = "";
 
-    for (let i = 0; i < number; i++){
-    let nameAgain = req.body[`name${i+1}`];
-    const isGoing = req.body[`checkbox${i+1}`] ? true : false;
+  // Loops through guests again to store their attendance status
+  for (let i = 0; i < number; i++) {
+    let nameAgain = req.body[`name${i + 1}`];
+    const isGoing = req.body[`checkbox${i + 1}`] ? true : false;
 
-    if (isGoing == true) {
-      going = "Yes"
-    }
-    else {
-      going = "No"
-    }
-    infos[nameAgain] = going;
+    going = isGoing ? "Yes" : "No"; // Converts attendance status to "Yes" or "No"
+    infos[nameAgain] = going; // Saves attendance info
   }
 
-
-  let noSingers = singers.length;
+  let noSingers = singers.length; // Counts how many guests are singing
 
   let pronoun = "";
   if (gender == "Male") pronoun = "he's";
   if (gender == "Female") pronoun = "she's"; 
-  
-  // declare the song line for the Jolly Good Fellow
+
+  // Defines the final song line for "For He's a Jolly Good Fellow"
   const songLine = `For,${pronoun},a,jolly,good,fellow.,For,${pronoun},a,jolly,good,fellow.,For,${pronoun},a,jolly,good,fellow,,which,nobody,can,deny!`;
 
-  // creates an array containing the guests and their corresponding word/lyric
+  // Creates an array of objects, assigning lyrics to guests in a cyclic manner
   const song = [
-    {singer: singers[0 % singers.length], word: "Happy"},
-    {singer: singers[1 % singers.length], word: "birthday"},
-    {singer: singers[2 % singers.length], word: "to"},
-    {singer: singers[3 % singers.length], word: "you."},
-    
-    {singer: singers[4 % singers.length], word: "Happy"},
-    {singer: singers[5 % singers.length], word: "birthday"},
-    {singer: singers[6 % singers.length], word: "to"},
-    {singer: singers[7 % singers.length], word: "you."},
-    
-    {singer: singers[8 % singers.length], word: "Happy"},
-    {singer: singers[9 % singers.length], word: "birthday"},
-    {singer: singers[10 % singers.length], word: "dear"},
-    {singer: singers[11 % singers.length], word: myName + "."},
-    
-    {singer: singers[12 % singers.length], word: "Happy"},
-    {singer: singers[13 % singers.length], word: "birthday"},
-    {singer: singers[14 % singers.length], word: "to"},
-    {singer: singers[15 % singers.length], word: "you!"},
-    
-    {singer: singers[16 % singers.length], word: songLine}
-  ]
+    { singer: singers[0 % singers.length], word: "Happy" },
+    { singer: singers[1 % singers.length], word: "birthday" },
+    { singer: singers[2 % singers.length], word: "to" },
+    { singer: singers[3 % singers.length], word: "you." },
 
-  res.render('happy', { myName, gender, number, names, song, noSingers, infos }); // Use "names" instead of "name"
+    { singer: singers[4 % singers.length], word: "Happy" },
+    { singer: singers[5 % singers.length], word: "birthday" },
+    { singer: singers[6 % singers.length], word: "to" },
+    { singer: singers[7 % singers.length], word: "you." },
+
+    { singer: singers[8 % singers.length], word: "Happy" },
+    { singer: singers[9 % singers.length], word: "birthday" },
+    { singer: singers[10 % singers.length], word: "dear" },
+    { singer: singers[11 % singers.length], word: myName + "." },
+
+    { singer: singers[12 % singers.length], word: "Happy" },
+    { singer: singers[13 % singers.length], word: "birthday" },
+    { singer: singers[14 % singers.length], word: "to" },
+    { singer: singers[15 % singers.length], word: "you!" },
+
+    { singer: singers[16 % singers.length], word: songLine }
+  ];
+
+  // Renders "happy.hbs" and passes all collected data
+  res.render("happy", { myName, gender, number, names, song, noSingers, infos });
 });
 
+// Renders the initial index page
 app.get("/", (req, res) => {
-  res.render("index")
+  res.render("index");
 });
 
-
-//Makes the app listen to port 3000
+// Makes the app listen to port 3000
 app.listen(3000, () => console.log(`App listening to port 3000`));
