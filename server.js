898854fd5abc6ac6e0d1e4cf6a9621ece73b5354
Christian <b2028crcsolidum@pshs.edu.ph>
2025-03-27 00:25:51 +0800

    final!!

--- server.js
+++ server.js
@@ -13,12 +13,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "hbs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 //Sets a basic route
 
 // Render the initial page with the number input form
-app.get("/", (req, res) => {
+app.get("/happy", (req, res) => {
   res.redirect("index"); // Ensure you have index.hbs
 });
 
@@ -29,7 +28,74 @@ app.get("/", (req, res) => {
 
 app.post("/happy", (req, res) => {
   const { myName, gender, number } = req.body;
-  res.render('happy', { myName, gender, number }); // Use "names" instead of "name"
+  let names = [];
+  let singers = [];
+
+  for (let i = 0; i<number; i++){
+      let name = req.body[`name${i+1}`];
+      names.push(name);
+      const isGoing = req.body[`checkbox${i+1}`] ? true : false; // checks whether a close friend/relative is going or not
+    if (isGoing == true) {
+      singers.push(name); // if the guest is going, add them to the singers array
+    }
+  }
+
+  let infos = {};
+  let going = ""
+
+    for (let i = 0; i < number; i++){
+    let nameAgain = req.body[`name${i+1}`];
+    const isGoing = req.body[`checkbox${i+1}`] ? true : false;
+
+    if (isGoing == true) {
+      going = "Yes"
+    }
+    else {
+      going = "No"
+    }
+    infos[nameAgain] = going;
+  }
+
+
+  let noSingers = singers.length;
+
+  let pronoun = "";
+  if (gender == "Male") pronoun = "he's";
+  if (gender == "Female") pronoun = "she's"; 
+  
+  // declare the song line for the Jolly Good Fellow
+  const songLine = `For,${pronoun},a,jolly,good,fellow.,For,${pronoun},a,jolly,good,fellow.,For,${pronoun},a,jolly,good,fellow,,which,nobody,can,deny!`;
+
+  // creates an array containing the guests and their corresponding word/lyric
+  const song = [
+    {singer: singers[0 % singers.length], word: "Happy"},
+    {singer: singers[1 % singers.length], word: "birthday"},
+    {singer: singers[2 % singers.length], word: "to"},
+    {singer: singers[3 % singers.length], word: "you."},
+    
+    {singer: singers[4 % singers.length], word: "Happy"},
+    {singer: singers[5 % singers.length], word: "birthday"},
+    {singer: singers[6 % singers.length], word: "to"},
+    {singer: singers[7 % singers.length], word: "you."},
+    
+    {singer: singers[8 % singers.length], word: "Happy"},
+    {singer: singers[9 % singers.length], word: "birthday"},
+    {singer: singers[10 % singers.length], word: "dear"},
+    {singer: singers[11 % singers.length], word: myName + "."},
+    
+    {singer: singers[12 % singers.length], word: "Happy"},
+    {singer: singers[13 % singers.length], word: "birthday"},
+    {singer: singers[14 % singers.length], word: "to"},
+    {singer: singers[15 % singers.length], word: "you!"},
+    
+    {singer: singers[16 % singers.length], word: songLine}
+  ]
+
+  res.render('happy', { myName, gender, number, names, song, noSingers, infos }); // Use "names" instead of "name"
+});
+
+app.get("/", (req, res) => {
+  res.render("index")
 });
 
 
